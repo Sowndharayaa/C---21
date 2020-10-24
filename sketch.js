61f695e56ae8f05d1767c835c88a4a4b2a6421f9
Sowndharayaa <71545646+Sowndharayaa@users.noreply.github.com>
2020-10-24 11:51:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var fixedRect, movingRect;
+var car,wall;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  car = createSprite(200,300,40,40);
+  car.shapeColor = "blue";
+  car.velocityX = 4;
+
+  wall = createSprite(1000,300,40,90);
+  wall.shapeColor = "pink";
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+
+  if(isTouching(car,wall)){
+    textSize(30);
+    fill("yellow");
+    text("It is touching",500,500);
+  }
+  collide(car,wall)
+
+  drawSprites();
+}
+
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2) {
+  return true;
+}
+else {
+  return false;
+}
+}
+
+function collide(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2) {
+      object1.setVelocity(0,0)
+}
+}
